47bdb888971c5307683114f443dee2bb00afb649
LazarevIvanISP242 <LazarevIvanOleg@mail.ru>
2026-09-12 10:59:00 +0300

    Step 7: variables and data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Metadata;
+
+string fn = "Ivan";
+string ln = "Lazarev";
+string g = "ISP-242";
+int by = 2000;
+double gpa = 4.5;
+bool hasSs = true;
+
+int cury = 2026;
+int age = cury - by;
+
+Console.WriteLine("Студенческое удостоверение");
+Console.WriteLine($"Имя: {fn} {ln}");
+Console.WriteLine($"Группа: {g}");
+Console.WriteLine($"Возраст: {age}");
+Console.WriteLine($"Средний балл: {gpa}");
+Console.WriteLine($"Стипендия: {hasSs}");
+
+Console.Write("\nВведите ваш любимый предмет: ");
+Console.WriteLine($"Отлично! {fn} любит {Console.ReadLine()}");
+
